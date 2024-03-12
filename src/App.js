8c07ed5e58584b26e3dd4196849bf9392dd93d48
Matej Kerekrety <matejkerekrety@datavant.com>
2024-03-12 21:21:00 +0000

    try HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { HashRouter as Router, Switch, Route, Link } from "react-router-dom";
 import RekolaRides from './RekolaRides';
 import Connectedness from './NetworkComponents';
 import Betweenness from './Betweenness';
