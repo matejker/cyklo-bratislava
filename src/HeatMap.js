b8c77fee1d5fa0a2918ba5437e7a265c1d593167
Matej Kerekrety <matejkerekrety@datavant.com>
2024-03-15 20:42:02 +0000

    returns and rents, mornign and evenings

--- src/HeatMap.js
+++ src/HeatMap.js
@@ -5,42 +5,81 @@ import "leaflet/dist/leaflet.css";
 import stations from './data/stations_clustered';
 import rents_clustered from './data/rents_clustered';
 import returns_clustered from './data/returns_clustered';
+
+import return_mornings from "./data/return_mornings";
+import return_evenings from "./data/return_evenings";
+import rent_evenings from "./data/rent_evenings";
+import rent_mornings from "./data/rent_mornings";
+
 import { ColorHeatmap, ColorHeatmapValues } from './colorHeatmap';
 import Toggle from './Toggle';
 
 
 const HeatMap = () => {
-    const rentValues = rents_clustered.map(v => v.reduce((partialSum, a) => partialSum + a, 0));
-    const returnValues = returns_clustered.map(v => v.reduce((partialSum, a) => partialSum + a, 0));
+    const rentMorningValues = rent_mornings.map(v => v.reduce((partialSum, a) => partialSum + a, 0));
+    const returnMorningValues = return_mornings.map(v => v.reduce((partialSum, a) => partialSum + a, 0));
+    const rentEveningValues = rent_mornings.map(v => v.reduce((partialSum, a) => partialSum + a, 0));
+    const returnEveningValues = return_evenings.map(v => v.reduce((partialSum, a) => partialSum + a, 0));
 
-    const [values, setValues] = useState(rentValues);
+    const [values, setValues] = useState(rentMorningValues);
     const [opposite, setOpposite] = useState(returns_clustered);
     const [index, setIndex] = useState(-1);
     const [colors, setColors] = useState(ColorHeatmap(values));
 
     const [checkedValue, setCheckedValue] = useState('Rents');
+    const [timeChecked, setTimeChecked] = useState('Mornings');
 
     useEffect(() => {
-        if (checkedValue === 'Rents') {
-            setColors(ColorHeatmap(rentValues));
-            setValues(rentValues);
-            setOpposite(returns_clustered);
-        } else {
-            setColors(ColorHeatmap(returnValues));
-            setValues(returnValues);
-            setOpposite(rents_clustered);
+        if (checkedValue === 'Rents' && timeChecked === 'Mornings') {
+            setColors(ColorHeatmap(rentMorningValues));
+            setValues(rentMorningValues);
+            setOpposite(return_mornings);
+        } else if (checkedValue === 'Rents' && timeChecked === 'Afternoons') {
+            setColors(ColorHeatmap(rentEveningValues));
+            setValues(rentEveningValues);
+            setOpposite(return_evenings);
+        } else if (checkedValue === 'Returns' && timeChecked === 'Mornings') {
+            setColors(ColorHeatmap(returnMorningValues));
+            setValues(returnMorningValues);
+            setOpposite(rent_mornings);
+        } else if (checkedValue === 'Returns' && timeChecked === 'Afternoons') {
+            setColors(ColorHeatmap(returnEveningValues));
+            setValues(returnEveningValues);
+            setOpposite(rent_evenings);
         }
-    }, [checkedValue]);
+    }, [checkedValue, timeChecked]);
 
     return (
         <>
-            <Toggle
-                values={['Rents', 'Returns']}
-                checkedValue={checkedValue}
-                setCheckedValue={setCheckedValue}
-                id={'toggle-mode'}
-                name={'toggle-mode'}
-            />
+            {/*<Toggle*/}
+            {/*    values={['Rents', 'Returns']}*/}
+            {/*    checkedValue={checkedValue}*/}
+            {/*    setCheckedValue={setCheckedValue}*/}
+            {/*    id={'toggle-mode'}*/}
+            {/*    name={'toggle-mode'}*/}
+            {/*/>*/}
+
+            {/*<Toggle*/}
+            {/*    values={['Mornings', 'Afternoons']}*/}
+            {/*    checkedValue={timeChecked}*/}
+            {/*    setCheckedValue={setTimeChecked}*/}
+            {/*    id={'toggle-time'}*/}
+            {/*    name={'toggle-time'}*/}
+            {/*/>*/}
+            <>
+                <ul className={'inline'}>
+                    <li><input type={'radio'} value={'rent'} id={'radio-rent'} checked={checkedValue === 'Rents'} onChange={() => setCheckedValue('Rents')} />
+                        <label htmlFor={'radio-rent'}>Rents</label></li>
+                    <li><input type={'radio'} value={'return'} id={'radio-return'} checked={checkedValue === 'Returns'} onChange={() => setCheckedValue('Returns')} />
+                        <label htmlFor={'radio-return'}>Returns</label></li>
+                {/*</ul>*/}
+                {/*<ul className={'inline'}>*/}
+                    <li><input type={'radio'} value={'morning'} id={'radio-morning'} checked={timeChecked === 'Mornings'} onChange={() => setTimeChecked('Mornings')} />
+                        <label htmlFor={'radio-morning'}>Mornings</label></li>
+                    <li><input type={'radio'} value={'afternoon'} id={'radio-afternoon'} checked={timeChecked === 'Afternoons'} onChange={() => setTimeChecked('Afternoons')} />
+                        <label htmlFor={'radio-afternoon'}>Afternoons</label></li>
+                </ul>
+            </>
 
             <Map style={{ height: "80vh" }} zoom={16} bounds={stations.map(s => s.location)}>
                     {
