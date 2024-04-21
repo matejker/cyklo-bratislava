bef640395cfc9ba4e37cac352637b954cf67ea58
Matej Kerekrety <matejkerekrety@datavant.com>
2024-04-21 16:27:26 +0100

    split the repos

--- src/Betweenness.js
+++ src/Betweenness.js
@@ -1,129 +1,169 @@
 import React, { useState } from "react";
-import { InlineMath, BlockMath } from 'react-katex';
+import { InlineMath, BlockMath } from "react-katex";
 import { Link } from "react-router-dom";
-import Toggle from './Toggle';
+import Toggle from "./Toggle";
 
-import strava_heatmap from './img/strava_heatmap.png';
-import slovnaft_bike_heatmap from './img/slovnaft_bike_heatmap.png';
-import edge_bc_cyclo from './img/edge_bc_cyclo.png';
-import edge_bc from './img/edge_bc.png';
+import strava_heatmap from "./img/strava_heatmap.png";
+import slovnaft_bike_heatmap from "./img/slovnaft_bike_heatmap.png";
+import edge_bc_cyclo from "./img/edge_bc_cyclo.png";
+import edge_bc from "./img/edge_bc.png";
 
 function Betweenness() {
-    const [mapMode, setMapMode] = useState("Bike");
+  const [mapMode, setMapMode] = useState("Bike");
 
-    return (
-        <>
-            <h2 id={'intro'}>3. Heatmaps and betweenness</h2>
-            <h3 id={"heatmaps"}>3.1. Heatmaps</h3>
-            <p>
-                A good proxy for a potentially suitable cycle path could be such a road segment which is
-                frequently used by other cyclists. We could say it <em>lies in between other routes</em> chosen
-                by cyclists. One approach is to look at heatmaps for instance as I did for
-                &nbsp;<Link to={"/rekola-rides#heatmap"}>Rekola rides</Link>, other Bratislava bike-sharing
-                platform <em>SlovnaftBike</em> or the global Strava heatmap.
-            </p>
+  return (
+    <>
+      <h2 id={"intro"}>3. Heatmaps and betweenness</h2>
+      <h3 id={"heatmaps"}>3.1. Heatmaps</h3>
+      <p>
+        A good proxy for a potentially suitable cycle path could be such a road
+        segment which is frequently used by other cyclists. We could say it{" "}
+        <em>lies in between other routes</em> chosen by cyclists. One approach
+        is to look at heatmaps for instance as I did for &nbsp;
+        <Link to={"/rekola-rides#heatmap"}>Rekola rides</Link>, other Bratislava
+        bike-sharing platform <em>SlovnaftBike</em> or the global Strava
+        heatmap.
+      </p>
 
-            <div className="center">
-                <img src={strava_heatmap} style={{width: '900px', marginLeft: '-100px'}} />
-                <div style={{textAlign: 'justify'}}>
-                <span className="image-desc">
-                    <a href={"https://www.strava.com/maps/global-heatmap?style=light&terrain=false&sport=Ride&gColor=bluered&gOpacity=100&labels=false&poi=false#11.73/48.1516/17.1121"}>Global Strava heatmap</a>
-                </span>
-                </div>
-            </div>
+      <div className="center">
+        <img
+          src={strava_heatmap}
+          style={{ width: "900px", marginLeft: "-100px" }}
+        />
+        <div style={{ textAlign: "justify" }}>
+          <span className="image-desc">
+            <a
+              href={
+                "https://www.strava.com/maps/global-heatmap?style=light&terrain=false&sport=Ride&gColor=bluered&gOpacity=100&labels=false&poi=false#11.73/48.1516/17.1121"
+              }
+            >
+              Global Strava heatmap
+            </a>
+          </span>
+        </div>
+      </div>
 
-            <p>
-                Even though all of those heatmaps shows which roads are frequently used by cyclists, they are all strongly skewed
-                based on the user base. For instance, Strava heatmap is more frequently used by recreational cyclists [?], while
-                bike-sharing platforms can be assumed to be used by commuters and tourists. In addition, bike-sharings are
-                determined by the cycle stations and their distribution.
-            </p>
+      <p>
+        Even though all of those heatmaps shows which roads are frequently used
+        by cyclists, they are all strongly skewed based on the user base. For
+        instance, Strava heatmap is more frequently used by recreational
+        cyclists [?], while bike-sharing platforms can be assumed to be used by
+        commuters and tourists. In addition, bike-sharings are determined by the
+        cycle stations and their distribution.
+      </p>
 
-            <div className="center">
-                <img src={slovnaft_bike_heatmap} style={{width: '900px', marginLeft: '-100px'}} />
-                <div style={{textAlign: 'justify'}}>
-                <span className="image-desc">
-                    Heatmap of SlovnaftBike bike-sharing system. The heatmap shows rides for one week.&nbsp;
-                    <a href={"https://geoportal.bratislava.sk/pfa/apps/sites/#/verejny-mapovy-portal/app/6f1aea97308b4e819781b4a71ed93249"}>Heatmapa jázd bicyklov SlovnaftBAjk za jeden týždeň</a>
-                </span>
-                </div>
-            </div>
+      <div className="center">
+        <img
+          src={slovnaft_bike_heatmap}
+          style={{ width: "900px", marginLeft: "-100px" }}
+        />
+        <div style={{ textAlign: "justify" }}>
+          <span className="image-desc">
+            Heatmap of SlovnaftBike bike-sharing system. The heatmap shows rides
+            for one week.&nbsp;
+            <a
+              href={
+                "https://geoportal.bratislava.sk/pfa/apps/sites/#/verejny-mapovy-portal/app/6f1aea97308b4e819781b4a71ed93249"
+              }
+            >
+              Heatmapa jázd bicyklov SlovnaftBAjk za jeden týždeň
+            </a>
+          </span>
+        </div>
+      </div>
 
-            <h3 id={"betweenness"}>3.2. Betweenness</h3>
-            <p>
-                Different approach would be to look at all the Bratislava city network as if cyclists from all points (nodes)
-                would like to go to all other points, ideally choosing the shortest paths. Then we could look at all
-                of the <em>streets</em> / links of the network and see which of them are the most frequently used, i.e.
-                which links lies <em>in between</em> the shortest paths. The ones which are more frequently used tend to
-                be more important.
-            </p>
+      <h3 id={"betweenness"}>3.2. Betweenness</h3>
+      <p>
+        Different approach would be to look at all the Bratislava city network
+        as if cyclists from all points (nodes) would like to go to all other
+        points, ideally choosing the shortest paths. Then we could look at all
+        of the <em>streets</em> / links of the network and see which of them are
+        the most frequently used, i.e. which links lies <em>in between</em> the
+        shortest paths. The ones which are more frequently used tend to be more
+        important.
+      </p>
 
-            <p>
-                In order to quantify this value, in network science, there is a concept of <em>betweenness centrality</em>
-                &nbsp;which is defined as
-                <BlockMath>{`l_i = \\sum_{st}\\frac{n ^ i_{st}}{g_{st}}`}</BlockMath>
-                where <InlineMath>{`g_{st}`}</InlineMath> is the total number of the shortest paths from node
-                &nbsp;<i>s</i> to node <i>t</i> and <InlineMath>{`n^i_{st}`}</InlineMath> is the number of those
-                paths that pass through link <i>i</i>. [1] There are many variations of this measure, the most common one
-                is node betweenness, which is the sum of all the shortest paths that pass through the node. Or random walk
-                betweenness, which is the number of times a random walk passes through a node. For other variations, see [2].
-            </p>
+      <p>
+        In order to quantify this value, in network science, there is a concept
+        of <em>betweenness centrality</em>
+        &nbsp;which is defined as
+        <BlockMath>{`l_i = \\sum_{st}\\frac{n ^ i_{st}}{g_{st}}`}</BlockMath>
+        where <InlineMath>{`g_{st}`}</InlineMath> is the total number of the
+        shortest paths from node &nbsp;<i>s</i> to node <i>t</i> and{" "}
+        <InlineMath>{`n^i_{st}`}</InlineMath> is the number of those paths that
+        pass through link <i>i</i>. [1] There are many variations of this
+        measure, the most common one is node betweenness, which is the sum of
+        all the shortest paths that pass through the node. Or random walk
+        betweenness, which is the number of times a random walk passes through a
+        node. For other variations, see [2].
+      </p>
 
-            <div className="center">
-                <Toggle
-                    values={['Bike', 'Car']}
-                    checkedValue={mapMode}
-                    setCheckedValue={setMapMode}
-                    id={'workday-toggle'}
-                    name={'workday-toggle'}
-                />
-                <img src={(mapMode === 'Bike') ? edge_bc_cyclo : edge_bc } style={{width: '1000px', marginLeft: '-150px'}} />
-                <div style={{textAlign: 'justify'}}>
-                    <span className="image-desc">
-                        Betweenness centrality of Bratislava city network. The darker the value, the more higher value has the edge
-                        betweenness. Switching between modes shows the difference between bike and car network.
-                    </span>
-                </div>
-            </div>
+      <div className="center">
+        <Toggle
+          values={["Bike", "Car"]}
+          checkedValue={mapMode}
+          setCheckedValue={setMapMode}
+          id={"workday-toggle"}
+          name={"workday-toggle"}
+        />
+        <img
+          src={mapMode === "Bike" ? edge_bc_cyclo : edge_bc}
+          style={{ width: "1000px", marginLeft: "-150px" }}
+        />
+        <div style={{ textAlign: "justify" }}>
+          <span className="image-desc">
+            Betweenness centrality of Bratislava city network. The darker the
+            value, the more higher value has the edge betweenness. Switching
+            between modes shows the difference between bike and car network.
+          </span>
+        </div>
+      </div>
 
-            <p>
-                Here, let's look at the betweenness centrality of Bratislava city network. We can see the betweenness
-                for car and bike network. I think the most obvious difference is the betweenness are the bridges over
-                Danube river. For cyclists is the <em>Most SNP</em> absolutely unattractive, while for cars it is the
-                complete opposite. Obviously, <em>Stary most</em> is not open for cars, other bridges are visibly build
-                more for cars than for cyclists.
-            </p>
+      <p>
+        Here, let's look at the betweenness centrality of Bratislava city
+        network. We can see the betweenness for car and bike network. I think
+        the most obvious difference is the betweenness are the bridges over
+        Danube river. For cyclists is the <em>Most SNP</em> absolutely
+        unattractive, while for cars it is the complete opposite. Obviously,{" "}
+        <em>Stary most</em> is not open for cars, other bridges are visibly
+        build more for cars than for cyclists.
+      </p>
 
-            <h3 id={"directness"}>3.3. Directness</h3>
-            <p>
-                The betweenness centrality of the network is a good proxy for the <em>directness</em> of the network. The
-                directness in network science means, how straight is the shortest path between points A and B on given
-                network compared to the <em>euclidean / shortest path on the graph</em>. This directness is measured for all possible
-                points A and B, and averaged over of all them. If we build cycle paths on the streets with high betweenness
-                centrality, we can expect that the network will be more direct and useful for commuting cyclists
-                &nbsp;<em>in general</em>.
-            </p>
+      <h3 id={"directness"}>3.3. Directness</h3>
+      <p>
+        The betweenness centrality of the network is a good proxy for the{" "}
+        <em>directness</em> of the network. The directness in network science
+        means, how straight is the shortest path between points A and B on given
+        network compared to the <em>euclidean / shortest path on the graph</em>.
+        This directness is measured for all possible points A and B, and
+        averaged over of all them. If we build cycle paths on the streets with
+        high betweenness centrality, we can expect that the network will be more
+        direct and useful for commuting cyclists &nbsp;<em>in general</em>.
+      </p>
 
-            <h3 id={"conclusion"}>3.4. Conclusion</h3>
+      <h3 id={"conclusion"}>3.4. Conclusion</h3>
 
-            <p>
-                Pretty much all of the heatmaps and betweenness centrality of the network are showing the same thing
-                and highlighting the roughly the same streets. It also shows that cyclist are using existing cycle paths,
-                which is clearly visible on the heatmap of SlovnaftBike bike-sharing and Strava.
-            </p>
+      <p>
+        Pretty much all of the heatmaps and betweenness centrality of the
+        network are showing the same thing and highlighting the roughly the same
+        streets. It also shows that cyclist are using existing cycle paths,
+        which is clearly visible on the heatmap of SlovnaftBike bike-sharing and
+        Strava.
+      </p>
 
-            <h3 className="reference">References:</h3>
-            <ol className="reference">
-                <li>
-                    Newman, M. E. J. (2010), <em>Networks: an introduction</em>, Oxford University Press, Oxford;
-                    New York
-                </li>
-                <li>
-                    Borgatti, S. P. (2005), <em>Centrality and network flow</em>, 27(1), 55–71. https://doi.org/10.1016/j.socnet.2004.11.008
-                </li>
-            </ol>
-        </>
-    )
+      <h3 className="reference">References:</h3>
+      <ol className="reference">
+        <li>
+          Newman, M. E. J. (2010), <em>Networks: an introduction</em>, Oxford
+          University Press, Oxford; New York
+        </li>
+        <li>
+          Borgatti, S. P. (2005), <em>Centrality and network flow</em>, 27(1),
+          55–71. https://doi.org/10.1016/j.socnet.2004.11.008
+        </li>
+      </ol>
+    </>
+  );
 }
 
 export default Betweenness;
--- src/Recommendation.js
+++ src/Recommendation.js
@@ -1,106 +1,133 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-import edge_betweeness_cyclo_paths from './img/edge_betweeness_cyclo_paths.png';
-import l2c_betweenness_existing from './img/l2c_betweenness_existing.png';
-import all_together from './img/all_together.png';
-import final from './img/final.png';
+import edge_betweeness_cyclo_paths from "./img/edge_betweeness_cyclo_paths.png";
+import l2c_betweenness_existing from "./img/l2c_betweenness_existing.png";
+import all_together from "./img/all_together.png";
+import final from "./img/final.png";
 
-function Recommendation () {
-    return (
-        <>
-            <h2>6. General recommendations</h2>
-            <p>
-                Models described in previous chapters are demonstrating important properties of a <em>good</em> cycle
-                network. First, Natera et.al. [1] showed how to increase <Link to={"/connectedness"}>network connectedness</Link>
-                &nbsp; by adding new links to the network. Then, Szell et.al. [2] showed how to increase
-                the <Link to={"/robustness"}>robustness</Link> and coverage using dense greedy triangulation, but it
-                doesn't consider already existing infrastructure. In this chapter, I will propose a general recommendation
-                and outline some routes for extending the network of cycle paths in Bratislava.
-            </p>
+function Recommendation() {
+  return (
+    <>
+      <h2>6. General recommendations</h2>
+      <p>
+        Models described in previous chapters are demonstrating important
+        properties of a <em>good</em> cycle network. First, Natera et.al. [1]
+        showed how to increase{" "}
+        <Link to={"/connectedness"}>network connectedness</Link>
+        &nbsp; by adding new links to the network. Then, Szell et.al. [2] showed
+        how to increase the <Link to={"/robustness"}>robustness</Link> and
+        coverage using dense greedy triangulation, but it doesn't consider
+        already existing infrastructure. In this chapter, I will propose a
+        general recommendation and outline some routes for extending the network
+        of cycle paths in Bratislava.
+      </p>
 
-            <h3>6.1. Directness</h3>
-            <p>
-                Probably the most important aspect of cycle path success is the its <em>usefulness</em>, in previous chapters
-                I demonstrated that a <em>good proxy</em> would be betweenness and directness of the network. First,
-                I would add the most important links to the existing network, i.e. the ones with the highest betweenness centrality.
-            </p>
+      <h3>6.1. Directness</h3>
+      <p>
+        Probably the most important aspect of cycle path success is the its{" "}
+        <em>usefulness</em>, in previous chapters I demonstrated that a{" "}
+        <em>good proxy</em> would be betweenness and directness of the network.
+        First, I would add the most important links to the existing network,
+        i.e. the ones with the highest betweenness centrality.
+      </p>
 
-            <div className="center">
-                <img src={edge_betweeness_cyclo_paths} style={{width: '1000px', marginLeft: '-150px'}} />
-                <div style={{textAlign: 'justify'}}>
-                <span className="image-desc">
-                    Blue - existing cycle paths, orange - new edges with high betweenness centrality.
-                </span>
-                </div>
-            </div>
+      <div className="center">
+        <img
+          src={edge_betweeness_cyclo_paths}
+          style={{ width: "1000px", marginLeft: "-150px" }}
+        />
+        <div style={{ textAlign: "justify" }}>
+          <span className="image-desc">
+            Blue - existing cycle paths, orange - new edges with high
+            betweenness centrality.
+          </span>
+        </div>
+      </div>
 
-            <h3>6.2. Connectedness</h3>
-            <p>
-                This network contains many of the most important links, but it is not connected. Therefore, to make it
-                connected, I used <em>Largest-to-closest</em> component algorithm proposed by Natera et.al. [1].
+      <h3>6.2. Connectedness</h3>
+      <p>
+        This network contains many of the most important links, but it is not
+        connected. Therefore, to make it connected, I used{" "}
+        <em>Largest-to-closest</em> component algorithm proposed by Natera
+        et.al. [1].
+      </p>
 
-            </p>
+      <div className="center">
+        <img
+          src={l2c_betweenness_existing}
+          style={{ width: "1000px", marginLeft: "-150px" }}
+        />
+        <div style={{ textAlign: "justify" }}>
+          <span className="image-desc">
+            Blue - existing cycle paths, orange - new edges with high
+            betweenness centrality.
+          </span>
+        </div>
+      </div>
 
-            <div className="center">
-                <img src={l2c_betweenness_existing} style={{width: '1000px', marginLeft: '-150px'}} />
-                <div style={{textAlign: 'justify'}}>
-                <span className="image-desc">
-                    Blue - existing cycle paths, orange - new edges with high betweenness centrality.
-                </span>
-                </div>
-            </div>
+      <h3>6.3. Robustness and coverage</h3>
+      <p>
+        To reinforce the links, we can connect the <em>death-end</em> paths with
+        the closest <em>death-end</em>. This will increase the robustness and
+        coverage of the network. This closes all the gaps in the network and its
+        directness overcomes the average discreteness of cars in this area.
+      </p>
 
-            <h3>6.3. Robustness and coverage</h3>
-            <p>
-                To reinforce the links, we can connect the <em>death-end</em> paths with the closest <em>death-end</em>. This
-                will increase the robustness and coverage of the network. This closes all the gaps in the network and
-                its directness overcomes the average discreteness of cars in this area.
-            </p>
+      <div className="center">
+        <img
+          src={all_together}
+          style={{ width: "1000px", marginLeft: "-150px" }}
+        />
+        <div style={{ textAlign: "justify" }}>
+          <span className="image-desc">
+            Blue - existing cycle paths, high betweenness and L2C links, red -
+            newly connected endpoints.
+          </span>
+        </div>
+      </div>
 
-            <div className="center">
-                <img src={all_together} style={{width: '1000px', marginLeft: '-150px'}} />
-                <div style={{textAlign: 'justify'}}>
-                <span className="image-desc">
-                    Blue - existing cycle paths, high betweenness and L2C links, red - newly connected endpoints.
-                </span>
-                </div>
-            </div>
+      <h3>6.4. Limitations</h3>
+      <p>
+        Even though the proposed strategy is based on the state-of-the-art
+        research, it is not perfect. It outlines how to build{" "}
+        <em>good enough</em> cyclo network, with significant margins for
+        improvement. Many of the paths are doubling already existing
+        infrastructure, and it is not clear how to prioritize the new links,
+        even strategies proposed in previous chapter could be an inspiration.
+      </p>
+      <div className="center">
+        <img src={final} style={{ width: "1000px", marginLeft: "-150px" }} />
+      </div>
 
-            <h3>6.4. Limitations</h3>
-            <p>
-                Even though the proposed strategy is based on the state-of-the-art research, it is not perfect. It outlines
-                how to build <em>good enough</em> cyclo network, with significant margins for improvement. Many of the paths
-                are doubling already existing infrastructure, and it is not clear how to prioritize the new links, even strategies
-                proposed in previous chapter could be an inspiration.
-            </p>
-            <div className="center">
-                <img src={final} style={{width: '1000px', marginLeft: '-150px'}} />
-            </div>
+      <h3>6.5. Conclusion</h3>
+      <p>
+        Main purpose of this project was to raise awareness of different
+        concept, treating cycle paths as form of network. To simulate two of the
+        models [1, 2] for extending the network of cycle paths specifically for
+        Bratislava, and to calculate and demonstrate centrality, betweenness and
+        directness. Last but not least, to describe some of the mobility
+        patterns of cyclists in Bratislava.
+      </p>
 
-
-            <h3>6.5. Conclusion</h3>
-            <p>
-                Main purpose of this project was to raise awareness of different concept, treating cycle paths as form of
-                network. To simulate two of the models [1, 2] for extending the network of cycle paths specifically for Bratislava, and
-                to calculate and demonstrate centrality, betweenness and directness. Last but not least, to describe
-                some of the mobility patterns of cyclists in Bratislava.
-            </p>
-
-            <h3 className="reference">References:</h3>
-            <ol className="reference">
-                <li>
-                    Natera, L.G., Battiston, F., Iñiguez, G., and Szell, M. (2019) <em>Data-driven strategies for
-                    optimal bicycle network growth</em>, <a href={"https://arxiv.org/abs/1907.07080"}>arXiv:1907.07080</a>
-                </li>
-                <li>
-                    Szell, M., Mimar, S., Perlman, T. et al. <em>Growing urban bicycle networks</em>.
-                    Sci Rep 12, 6765 (2022).
-                    &nbsp; <a href={"https://doi.org/10.1038/s41598-022-10783-y"}>https://doi.org/10.1038/s41598-022-10783-y</a>
-                </li>
-            </ol>
-        </>
-    )
+      <h3 className="reference">References:</h3>
+      <ol className="reference">
+        <li>
+          Natera, L.G., Battiston, F., Iñiguez, G., and Szell, M. (2019){" "}
+          <em>Data-driven strategies for optimal bicycle network growth</em>,{" "}
+          <a href={"https://arxiv.org/abs/1907.07080"}>arXiv:1907.07080</a>
+        </li>
+        <li>
+          Szell, M., Mimar, S., Perlman, T. et al.{" "}
+          <em>Growing urban bicycle networks</em>. Sci Rep 12, 6765 (2022).
+          &nbsp;{" "}
+          <a href={"https://doi.org/10.1038/s41598-022-10783-y"}>
+            https://doi.org/10.1038/s41598-022-10783-y
+          </a>
+        </li>
+      </ol>
+    </>
+  );
 }
 
 export default Recommendation;
